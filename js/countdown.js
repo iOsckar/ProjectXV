999a60e03a4cecffe13827a299c9fc9934b974cf
iOsckar <oscarandres131@hotmail.com>
2025-02-07 20:05:11 -0600

    Countdown section added

--- js/countdown.js
+++ js/countdown.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 9, 2025 17:30:00").getTime();
+var countDownDate = new Date("Mar 29, 2025 17:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -17,8 +17,8 @@ var x = setInterval(function() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
   // Output the result in an element with id="demo"
-  document.getElementById("countdown-text").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
+  document.getElementById("countdown-text").innerHTML = "<p>" + days + " : " + hours + " : "
+  + minutes + " : " + seconds + " </p>";
     
   // If the count down is over, write some text 
   if (distance < 0) {
